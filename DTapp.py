cdbd290494d13bef8727235bf872f21db3668e45
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-01 09:09:24 +0900

    windowに反映させる

--- DTapp.py
+++ DTapp.py
@@ -6,4 +6,8 @@ from tkinter import messagebox as mbox
 window=tk.Tk()
 window.geometry("300x100")
 window.title("Excelをいじる")
+
+label=tk.Label(window,text="入力するのだ")
+label.pack()
+
 window.mainloop()
